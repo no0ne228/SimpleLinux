e6388ab096428a02add4e50685cf7ca812bcf8cc
KolibriKing12 <maksimevstratiev12@outlook.com>
2023-11-27 15:03:06 +0200

    Added clear command

--- usr/bin/sh.h
+++ usr/bin/sh.h
@@ -4,12 +4,22 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void sh_run(const char command[]) {
   if (strcmp(command, "welcome") == 0) {
     printf("Welcome to SimpleLinux!\n\n");
-    printf("This system is created to make Linux simpler and smaller, so it is "
-           "actually worst than Linux.\nIt runs right in your terminal so you "
-           "don't need to replace your original OS with this.\n");
+    printf("This system is created to make Linux simpler and smaller, so it is actually worst than Linux.\nIt runs right in your terminal so you don't need to replace your original OS with this.\n");
+    printf("Type 'help' to see avaliable commands");
+  } else if (strcmp(command, "help") == 0) {
+    printf("clear - clears the terminal\n");
+  } else if (strcmp(command, "clear") == 0) {
+    #ifdef _WIN32
+    system("cls");
+    #elif __APPLE__
+    printf("\033c");
+    #elif __linux__
+    printf("\033c");
+    #endif
   }
 }
